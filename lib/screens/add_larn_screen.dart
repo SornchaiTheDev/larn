a7b55afc2c7a1ee61e1afb445b72b032f30a5888
SornchaiTheDev <sornchaithedev@gmail.com>
2024-06-26 22:09:30 +0700

    refactor: add larn screen

--- lib/screens/add_larn_screen.dart
+++ lib/screens/add_larn_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:larn/constants/colors.dart';
-import 'package:larn/widgets/apps_icon_widget.dart';
+import 'package:larn/widgets/add_larn_widget.dart';
 import 'package:larn/widgets/back_button_widget.dart';
 
 class AddLarnScreen extends StatelessWidget {
@@ -10,142 +8,58 @@ class AddLarnScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
+      body: Column(
+        children: [
+          const TopNavigation(),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 40),
+              itemCount: 10,
+              itemBuilder: (context, index) => const AddLarnWidget(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TopNavigation extends StatelessWidget {
+  const TopNavigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 48),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: BackButtonWidget(),
-                  ),
-                  Text(
-                    "เพิ่มหลาน",
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                  Expanded(
-                    child: SizedBox(
-                      width: 10,
-                    ),
-                  )
-                ],
+            Expanded(
+              child: BackButtonWidget(),
+            ),
+            Text(
+              "เพิ่มหลาน",
+              style: TextStyle(
+                fontSize: 22,
               ),
             ),
-            const SizedBox(height: 20),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: ListView(
-                  clipBehavior: Clip.antiAlias,
-                  children: [
-                    Ink(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          color: Colors.black26,
-                          width: 2,
-                        ),
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(14.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const CircleAvatar(
-                                  radius: 30,
-                                  backgroundImage:
-                                      AssetImage("assets/images/larn1.png"),
-                                ),
-                                const SizedBox(
-                                  width: 24,
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const Text(
-                                        "หลานภาษา",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 6),
-                                      const Text(
-                                        "หลานบันเทิงจะช่วยให้คุณ ใช้งานแอปพลิเคชันความบันเทิงได้ง่ายขึ้น",
-                                      ),
-                                      const SizedBox(height: 12),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                const Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("แอปที่สอน"),
-                                    SizedBox(height: 6),
-                                    AppsIconWidget()
-                                  ],
-                                ),
-                                InkWell(
-                                  onTap: () {},
-                                  splashColor: Colors.amber,
-                                  borderRadius: const BorderRadius.all(
-                                    Radius.circular(10.0),
-                                  ),
-                                  child: Ink(
-                                    padding: const EdgeInsets.fromLTRB(
-                                        24, 12, 24, 12),
-                                    decoration: const BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(10.0),
-                                      ),
-                                      color: primaryColor,
-                                    ),
-                                    child: const Row(
-                                      children: [
-                                        FaIcon(
-                                          FontAwesomeIcons.plus,
-                                          size: 14,
-                                        ),
-                                        SizedBox(width: 10),
-                                        Text(
-                                          "เพิ่มหลาน",
-                                          style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+              child: SizedBox(
+                width: 10,
               ),
             )
           ],
---
+++ lib/widgets/add_larn_widget.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:larn/constants/colors.dart';
+import 'package:larn/widgets/apps_icon_widget.dart';
+
+class AddLarnWidget extends StatelessWidget {
+  const AddLarnWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Ink(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.black26,
+              width: 2,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage("assets/images/larn1.png"),
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "หลานภาษา",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const SizedBox(height: 6),
+                          const Text(
+                            "หลานบันเทิงจะช่วยให้คุณ ใช้งานแอปพลิเคชันความบันเทิงได้ง่ายขึ้น",
+                          ),
+                          const SizedBox(height: 12),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("แอปที่สอน"),
+                        SizedBox(height: 6),
+                        AppsIconWidget()
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      splashColor: Colors.amber,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                      child: Ink(
+                        padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                          color: primaryColor,
+                        ),
+                        child: const Row(
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.plus,
+                              size: 14,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "เพิ่มหลาน",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
